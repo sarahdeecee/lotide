9b649bc23457cee352744d1a6293bec2747c024d
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-10-27 18:22:38 -0400

    countLetters function counts the # of letters in a string and returns on object

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`⭐️ Assertion Passed: ${actual} === ${expected} ⭐️`)
+    : console.log(`🌧 Assertion Failed: ${actual} !== ${expected} 🌧`);
+};
+
+// const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const countLetters = (sentence) => {
+  // return count of letters
+  const results = {};
+  const noSpaces = sentence.split(" ").join(""); 
+  for (let char of noSpaces) {
+    if (results[char]) {
+      results[char] += 1;
+    } else {
+      results[char] = 1;
+    }
+  }
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"));
