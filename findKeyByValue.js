95cf25579438afc46fac5fa9b16d6f393a321753
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-10-27 18:53:34 -0400

    findKeyByValue function returns the property name if a given object has a given value

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`⭐️ Assertion Passed: ${actual} === ${expected} ⭐️`)
+    : console.log(`🌧 Assertion Failed: ${actual} !== ${expected} 🌧`);
+};
+
+const findKeyByValue = (obj, val) => {
+  let result = "";
+  for (let prop in obj) {
+    if (obj[prop] === val) {
+      return prop;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
