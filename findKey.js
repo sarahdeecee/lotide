9caac6ff8347a8eb0360780d3f5f4f8c7b331cff
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-10-28 18:28:30 -0400

    findKey function returns the first key in an object whose values pass a criteria

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`⭐️ Assertion Passed: ${actual} === ${expected} ⭐️`)
+    : console.log(`🌧 Assertion Failed: ${actual} !== ${expected} 🌧`);
+};
+
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+const expected = "noma";
+assertEqual(test,expected);
