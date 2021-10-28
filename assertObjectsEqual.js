980e0507479e9fba2088e393c6505c90b9cfd1df
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-10-27 20:17:58 -0400

    implemented assertObjectsEqual function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -28,3 +28,20 @@ const eqObjects = (obj1, obj2) => {
     return true;
   }
 };
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  (eqObjects(actual, expected)) ? console.log(`⭐️ Assertion Passed: These objects are equal! ${inspect(actual)} === ${inspect(expected)} ⭐️`)
+    : console.log(`🌧 Assertion Failed: These objects are not equal. ${inspect(actual)} !== ${inspect(expected)} 🌧`);
+};
+
+// const test1 = { color: "red", size: "small" };
+// const test2 = { size: "small", color: "red" };
+// const test3 = { size: "small", color: "blue" };
+// const testArr1 = { color: ["red", "white"], size: "small" };
+// const testArr2 = { size: "small", color: ["red", "white"] };
+// test1Props = Object.keys(test1);
+// console.log(`Testing identical objects: \n${assertObjectsEqual(test1, test2)}`);
+// console.log(`Testing non-identical objects: \n${assertObjectsEqual(test1, test3)}`);
+// console.log(`Testing array values: \n${assertObjectsEqual(testArr1, testArr2)}`);
