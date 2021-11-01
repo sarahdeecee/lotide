d3038ebfec000db0f5e5e3f46ac3f859fe3c092f
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-01 14:54:11 -0400

    refectored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,25 +1,8 @@
-//writes an error message to console if false (if true -> nothing)
-const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log(`⭐️ Assertion Passed: ${actual} === ${expected} ⭐️`)
-    : console.log(`🌧 Assertion Failed: ${actual} !== ${expected} 🌧`);
-};
-
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-    if (i === arr1.length - 1) {
-      return true;
-    }
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   (eqArrays(arr1, arr2)) ? console.log(`⭐️ Assertion Passed: These arrays are equal! ⭐️`)
   : console.log(`🌧 Assertion Failed: These arrays are not equal. 🌧`);
 }
 
-// assertArraysEqual([1, 2, 3], [1, 2, 3]);
-// assertArraysEqual([1, 2, 3], [1, 2]);
-// assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+module.exports = assertArraysEqual;
